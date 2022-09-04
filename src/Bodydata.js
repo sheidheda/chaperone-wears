bf96d8b217679fc6953678d9a4875977731ce952
Faithful <faithfulmiracleajah@gmail.com>
2022-09-04 21:36:29 +0100

    deployed website

--- src/Bodydata.js
+++ src/Bodydata.js
@@ -16,7 +16,7 @@ export default [
     },
     {
         id: 3,
-        image: "https:images.unsplash.com/photo-1578758837674-93ed0ab5fbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        image: "https://images.unsplash.com/photo-1578758837674-93ed0ab5fbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
         Text: "Are you looking for a suit that will make you look stunning and express your personal style to the fullest? Chaperone has the answer! Our suits are not only high-quality, they are also hand tailored to your measurements.",
         buttonText: "Learn More",
         
